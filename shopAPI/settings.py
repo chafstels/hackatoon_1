2ae13102b6d6e8b34a2bee6ef4baec330be927cc
Anton <anton@Anton-MacBook-Pro.local>
2023-07-27 14:11:39 +0600

    CORS

--- shopAPI/settings.py
+++ shopAPI/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'twilio',
+    "corsheaders",
 
     #myapp
     'account',
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "shopAPI.urls"
